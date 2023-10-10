a196a64fd048a62895d6a3c4ebb304e759dd2d23
Sorin C <sorin.cucereavu@gmail.com>
2023-10-10 10:38:13 +0300

    Add changes

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,20 +2,30 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
-    private WebDriver driver=null;
-    private By login=By.xpath("//*[@id=\"ap_email\"]");
-    private By password=By.xpath( "//*[@id=\"ap_password\"]");
+     WebDriver driver;
+     //By logins=By.id("\"ap_email\"");
+    // By passwordLogin=By.xpath( "//*[@id=\"ap_password\"]");
+
+    @FindBy(id = "\"ap_email\"")
+    WebElement logins;
+
+    @FindBy(xpath = "//*[@id=\"ap_password\"]")
+    WebElement passwordLogin;
 
     public LoginPage(WebDriver driver){
         this.driver=driver;
     }
 
     public void setLogin(String login){
-        driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys(login);
+        logins.sendKeys(login);
     }
     public void setPassword(String password) {
-        driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys(password);
+       passwordLogin.sendKeys(password);
     }
+
 }
